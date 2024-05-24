af85006e2a26e7731a4148c14953ab9519f9c5e9
6a61737065725f <84739617+6a61737065725f@users.noreply.github.com>
2024-05-23 18:52:02 -0700

    added code for VPC stack generation to CloudFormation

--- src/main/java/com/myorg/EcommerceEcsCdkApp.java
+++ src/main/java/com/myorg/EcommerceEcsCdkApp.java
@@ -3,8 +3,12 @@ package com.myorg;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.model.GetCallerIdentityRequest;
+import software.amazon.awssdk.services.sts.model.GetCallerIdentityResponse;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,16 +16,30 @@ public class EcommerceEcsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
+        StsClient stsClient = StsClient.builder()
+                .region(Region.US_WEST_1)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+
+        GetCallerIdentityResponse response = stsClient.getCallerIdentity(GetCallerIdentityRequest.builder().build());
+        String accountId = response.account();
+
         Environment environment = Environment.builder()
-                .account("218821328974")
-                .region("us-west-1")
+                .account(accountId)
+                .region(Region.US_WEST_1.toString())
                 .build();
 
         Map<String, String> infraTags = new HashMap<>();
-        infraTags.put("team", "project");
-        infraTags.put("cost", "ECommerceInfra");
+        infraTags.put("team", "PersonalProject");
+        infraTags.put("cost", "ECommerceInfrastructure");
 
         EcrStack ecrStack = new EcrStack(app, "Ecr", StackProps.builder()
+                .env(environment)
+                .tags(infraTags)
+                .build());
+
+        // Destroy this stack to reduce infrastructure costs since NAT gateways incur costs
+        VpcStack vpcStack = new VpcStack(app, "Vpc", StackProps.builder()
                 .env(environment)
                 .tags(infraTags)
                 .build());
